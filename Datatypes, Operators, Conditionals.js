f0f739b159da43be526e7dc181109bc15ace147a
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-05 16:35:07 +0200

    finished and running

--- Datatypes, Operators, Conditionals.js
+++ Datatypes, Operators, Conditionals.js
@@ -11,7 +11,7 @@ ASSIGNMENT RULES
 /* EXERCISE 1
  Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
 */
-/*
+
 //  The number datatype will run a specific number as belwow, can be any number up to 15 digits
 let number =7  
 console.log(number)
@@ -38,7 +38,7 @@ let hand = 22
 //  Null used to empty datatpe there is nothing there like below
 let extream = null
 console.log (extream)
-*/
+
 
  
 
@@ -46,55 +46,55 @@ console.log (extream)
  Try to describe what a variable is, in your own words.
 */
 
-/* 
+
 // A Variable is a place to store data as below. crime is the variable
-let crime = punishment
+let crime = "punishment"
 console.log (crime)
 
-*/
+
 
 /* EXERCISE 3
  Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
 
-/* 
+ 
 let first = 12
 let second = 20
 let sum = (first + second)
 console.log (first + second)
 console.log (sum)
 
-*/
+
 
 /* EXERCISE 4
  Create a variable named x and assign to it the number 12.
 */
 
-/* let x = 12
+ let x = 12
 console.log (x)
-*/
+
 
 /* EXERCISE 5
  Create a variable called name and assign to it the string "John Doe".
 */
 
-/* 
+
 let name ="John Doe"
 console.log (name)
 
-*/
+
 
 /* EXERCISE 6
  Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
 */
 
-/* 
-let theman = 12
-let sum = (x - theman)
-console.log (x - theman)
-console.log (sum)
 
-*/
+let theman = 12
+let amount = (x - theman)
+console.log (x - theman)
+console.log (amount)
+
+
 
 /* EXERCISE 7
  Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
@@ -102,36 +102,34 @@ console.log (sum)
  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
 */
 
-/* 
-let name1 = "john"
-let name2 = "John"
-console.log (name1===name2)
-console.log (name1!==name2)
 
-*/
+let nam1 = "john"
+let nam2 = "John"
+console.log (nam1===nam2)
+console.log (nam1!==nam2)
+
+
 
 /* EXERCISE 8
  Create a variable named x, and assign to it a numeric value less than 10.
  Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
 */
 
-/* 
 
-let x = 9
+
+
 let y = "nine"
 if (x===9) {y}
 console.log (y)
 
-*/
+
 
 /* EXERCISE 9
  [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
  try to search it by yourself, tomorrow morning we'll discuss it together!)
 */
 
-/* 
+ 
 let age = 60
 let socialSecurity = age >= 65 ? "pension" : "noPension"
 console.log (socialSecurity)
-
-*/
