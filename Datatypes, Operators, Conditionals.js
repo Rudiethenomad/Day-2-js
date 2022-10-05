14442c9273783dbd61a6270fdb41ca11c6fb6768
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-05 15:38:54 +0200

    Almost finished

---
+++ Datatypes, Operators, Conditionals.js
@@ -0,0 +1,130 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+/*
+//  The number datatype will run a specific number as belwow, can be any number up to 15 digits
+let number =7  
+console.log(number)
+
+// Undefined indicates when a variable has not been assigned a value, or not declared at all like below. there is nothing after like (= 20) so it will return a result of undefined.
+let Jon
+console.log(Jon)
+
+// Object are a collection of the key value properites as the example below and will return the result of the variables added together
+ let Operators = (6 + 1)
+console.log(Operators)
+
+// Strings are words or character inside " " like in the example below
+let string = "HomeworkIsAwsome"
+console.log(string)
+
+// Boolean is a something that can only be answered as yes or no as below
+
+ let  jackpot = true
+ let bust = false
+let hand = 22 
+ console.log(jackpot)
+
+//  Null used to empty datatpe there is nothing there like below
+let extream = null
+console.log (extream)
+*/
+
+ 
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* 
+// A Variable is a place to store data as below. crime is the variable
+let crime = punishment
+console.log (crime)
+
+*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+/* 
+let first = 12
+let second = 20
+let sum = (first + second)
+console.log (first + second)
+console.log (sum)
+
+*/
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+/* let x = 12
+console.log (x)
+*/
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/* 
+let name ="John Doe"
+console.log (name)
+
+*/
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* 
+let theman = 12
+let sum = (x - theman)
+console.log (x - theman)
+console.log (sum)
+
+*/
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+/* 
+let name1 = "john"
+let name2 = "John"
+console.log (name1===name2)
+console.log (name1!==name2)
+
+*/
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+/* 
+let x = (9)
+
+
+
+*/
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+/* WRITE YOUR ANSWER HERE */
---
+++ dj.js
@@ -0,0 +1,84 @@
+/*
+//  The number datatype will run a specific number as belwow, can be any number up to 15 digits
+let number =7  
+console.log(number)
+
+// Undefined indicates when a variable has not been assigned a value, or not declared at all like below. there is nothing after like (= 20) so it will return a result of undefined.
+let Jon
+console.log(Jon)
+
+// Object are a collection of the key value properites as the example below and will return the result of the variables added together
+ let Operators = (6 + 1)
+console.log(Operators)
+
+// Strings are words like in the example below
+let string = "HomeworkIsAwsome"
+console.log(string)
+
+// Boolean is a something that can only be answered as yes or no as below
+
+ let  jackpot = true
+ let bust = false
+let hand = 22 
+ console.log(jackpot)
+
+//  Null used to empty datatpe there is nothing there like below
+let extream = null
+console.log (extream)
+*/
+ 
+/* 
+// A Variable is a place to store data as below. crime is the variable
+let crime = punishment
+console.log (crime)
+
+*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+/* 
+let first = 12
+let second = 20
+let sum = (first + second)
+console.log (first + second)
+console.log (sum)
+
+*/
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+/*
+ let x = 12
+console.log (x)
+
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/*
+let name ="John Doe"
+console.log (name)
+*/
+
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+/*
+let theman = 12
+let sum = (x - theman)
+console.log (x - theman)
+console.log (sum)
+
+*/
+/*
+let name1 = "john"
+let name2 = "John"
+console.log (name1===name2)
+console.log (name1!==name2)
+*/
